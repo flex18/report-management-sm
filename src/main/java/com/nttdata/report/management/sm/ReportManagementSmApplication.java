9743a7b9a6b4bb83fdfed90f15b35e1259e7de3b
flex18 <emendozaminchan@gmail.com>
2024-03-04 14:30:13 -0500

    add structure ms

---
+++ src/main/java/com/nttdata/report/management/sm/ReportManagementSmApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.report.management.sm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportManagementSmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportManagementSmApplication.class, args);
+	}
+
+}
